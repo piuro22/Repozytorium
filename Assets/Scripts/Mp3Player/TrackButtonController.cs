6f3680e55a6fabf58b8cb905bcf31d89b56cb7c5
adam.zadorski97@gmail.com <adam.zadorski97@gmail.com>
2024-12-04 17:30:45 +0100

    mp3

--- Assets/Scripts/Mp3Player/TrackButtonController.cs
+++ Assets/Scripts/Mp3Player/TrackButtonController.cs
@@ -1,36 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class TrackButtonController : MonoBehaviour
 {
     private Mp3TrackProperties mp3TrackProperties;
+
     [SerializeField] private TMP_Text textTitle;
+
+    /// <summary>
+    /// Initializes the track button with track properties.
+    /// </summary>
+    /// <param name="mp3TrackProperties">Properties of the track.</param>
     public void Initialize(Mp3TrackProperties mp3TrackProperties)
     {
         this.mp3TrackProperties = mp3TrackProperties;
-        textTitle.SetText(mp3TrackProperties.title);
+        UpdateTitle(mp3TrackProperties.title);
     }
 
-   /* public void OnButtonClick()
+    /// <summary>
+    /// Updates the title text of the button.
+    /// </summary>
+    /// <param name="title">Title to display on the button.</param>
+    private void UpdateTitle(string title)
     {
-        Mp3PlayerController.Instance.PlayTrack(mp3TrackProperties.title);
-    }
-   */
-      public void HighLightButton(bool state)
-    {
-        if (state == false)
+        if (textTitle != null)
         {
-            string currentText = textTitle.text;
-            string newText = currentText.Replace("<b>", "").Replace("</b>", "");
-            textTitle.text = newText;
+            textTitle.SetText(title);
         }
-        else
+    }
+
+    /// <summary>
+    /// Handles the button click event to play the associated track.
+    /// </summary>
+    public void OnButtonClick()
+    {
+        if (mp3TrackProperties != null)
         {
-            string currentText = textTitle.text;
-            string newText = "<b>" + currentText + "</b>";
-            textTitle.text = newText;
+            Mp3PlayerController.Instance.PlayTrackByTitle(mp3TrackProperties.title);
+        }
+    }
+
+    /// <summary>
+    /// Highlights or unhighlights the button text based on the state.
+    /// </summary>
+    /// <param name="state">True to highlight, false to unhighlight.</param>
+    public void HighLightButton(bool state)
+    {
+        if (textTitle != null)
+        {
+            if (state)
+            {
+                textTitle.text = $"<b>{mp3TrackProperties.title}</b>";
+            }
+            else
+            {
+                textTitle.text = mp3TrackProperties.title;
+            }
         }
     }
 }
-
